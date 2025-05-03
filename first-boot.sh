2340e37e227bc952b062600d173960a0798a8e9b
gitovski <gitovski@bitsecure.email>
2025-05-04 00:43:19 +0200

    update

--- first-boot.sh
+++ first-boot.sh
@@ -75,8 +75,10 @@ echo "Root Script Starting..."
 # Root the VM
 git clone https://gitlab.com/newbit/rootAVD.git
 pushd rootAVD
-sed -i 's/choice=1/choice=2/g' rootAVD.sh
-yes 2 | ./rootAVD.sh system-images/android-30/default/x86_64/ramdisk.img FAKEBOOTIMG
+echo "************SELECTIIIIIIIIING MAGIIIIISK*****************"
+sed -i '/read -t 10 choice/{n;s/choice=1/choice=2/;}' rootAVD.sh
+./rootAVD.sh system-images/android-30/default/x86_64/ramdisk.img FAKEBOOTIMG
+s
 cp /opt/android-sdk/system-images/android-30/default/x86_64/ramdisk.img /data/android.avd/ramdisk.img
 popd
 echo "Root Done"
